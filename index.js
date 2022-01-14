ec0d125a9e4ba60eef70b01e52e41618df2a48b9
Egor Mironov <doramifasolyasi@gmail.com>
2022-01-14 20:10:59 +0400

    feat: make a layout

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const keyboard = document.querySelector('.keyboard__block')
 
 document.addEventListener('keydown', function(event){
-    console.log(event)
-    if(event.code == 'Space'){
-        const space = document.querySelector('.Space')
-        space.classList.add('active')
-    }
+    console.log(event.code)
+        const space = document.querySelector(event.code)
+        space?.classList.add('active')
 })
 
 document.addEventListener('keyup', function(event){
     console.log(event)
-    if(event.code == 'Space'){
-        const space = document.querySelector('.Space')
-        space.classList.remove('active')
-    }
+    const keyLight = document.querySelector(event.code)
+    keyLight?.classList.remove('active')
 })
 
 
