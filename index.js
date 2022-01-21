e1fa4b93e5ce125048acdbec6e4bf51edafb60ed
Egor Mironov <doramifasolyasi@gmail.com>
2022-01-21 20:19:27 +0400

    feat: my layout of keyboard

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 const keyboard = document.querySelector('.keyboard__block')
+localStorage.setItem('lang', 'ru');
+localStorage.setItem('size', 'small')
 
+
+    document.addEventListener('keydown', function(event){
+        if(event.code === 'ShiftRight'  && localStorage.getItem('lang') === 'ru'){
+                localStorage.setItem('lang', 'en')
+        }else if (event.code === 'ShiftRight' && localStorage.getItem('lang') === 'en'){
+                localStorage.setItem('lang', 'ru')
+        }
+        addActiveBtn()
+    })
+  
+    
+    document.addEventListener('keydown', function(event){
+        if(event.code === 'CapsLock' && localStorage.getItem('size') === 'small'){
+            localStorage.setItem('size', 'big')
+        }else if(event.code === 'CapsLock' && localStorage.getItem('size') === 'big'){
+            localStorage.setItem('size', 'small')
+        }
+        addActiveBtn()
+    })
+    document.addEventListener('keydown', function(event){
+        if(event.code === 'ShiftLeft' && localStorage.getItem('size') === 'small'){
+            localStorage.setItem('size', 'big')
+        }
+        addActiveBtn()
+    })
+
+    document.addEventListener('keyup', function(event){
+        if(event.code === 'ShiftLeft' && localStorage.getItem('size') === 'big'){
+            localStorage.setItem('size', 'small')
+        }
+        addActiveBtn()
+    })
+    
 document.addEventListener('keydown', function(event){
-    console.log(event.code)
-        const space = document.querySelector(event.code)
-        space?.classList.add('active')
-})
+    console.log(event)
+    const keyLight = document.querySelector(`.${event.code}`)
+    keyLight?.classList.add('active')
+});
 
 document.addEventListener('keyup', function(event){
-    console.log(event)
-    const keyLight = document.querySelector(event.code)
-    keyLight?.classList.remove('active')
-})
+    const keyLight = document.querySelector(`.${event.code}`)
+    if(event.code === 'CapsLock' && localStorage.getItem('size') === 'big'){
+        return 
+    }
+        keyLight?.classList.remove('active')
+});
 
 
-
-
+function addActiveBtn(){
+    const size = localStorage.getItem('size')
+    const lang = localStorage.getItem('lang')
+    const collection = document.querySelectorAll(`[${size}-key-${lang}]`)
+    const allItems = document.querySelectorAll('.hidden')
+    for(let i = 0; i<allItems.length;i++){
+        allItems[i].classList.remove('active_btn')
+    }
+    for(let i = 0; i<collection.length;i++){
+        collection[i].classList.add('active_btn')
+    }
+}
+    
